f4bde39f6fe374de9a3b09886fc41fe9bea30832
Piers Beckley <piers@fatpigeons.com>
2022-08-23 16:27:36 +0100

    cut down on the fluff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/sh -l
 
-#bash -c "$(cp ' %q' "$@")"
-
-echo "hello"
->&2 echo "test debug message"
-
-time=$(date)
-echo "::set-output name=time::$time"
-
-
-ENV ARG_1=$1
-ENV ARG_2=$2
-ENV ARG_3=$3
-
-
 echo $1 >> /tmp/logs.txt
 echo $2 >> /tmp/logs.txt
 echo $3 >> /tmp/logs.txt
@@ -22,4 +8,4 @@ echo >> /tmp/logs.txt "Receiving " $# " arguments"
 
 echo "trying to run cp "  "$2 " "$3"
 
-#cp "$2" "$3"
+cp "$2" "$3"
