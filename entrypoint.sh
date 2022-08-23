fe951f98d5d67607139866492b6249bae49510b1
Piers Beckley <piers@fatpigeons.com>
2022-08-23 16:29:07 +0100

    This should theoretically work!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh -l
 
-echo $1 >> /tmp/logs.txt
-echo $2 >> /tmp/logs.txt
-echo $3 >> /tmp/logs.txt
-
-echo >> /tmp/logs.txt "Receiving " $# " arguments"
-
-echo "trying to run cp "  "$2 " "$3"
-
-cp "$2" "$3"
+cp "$1" "$2" "$3"
